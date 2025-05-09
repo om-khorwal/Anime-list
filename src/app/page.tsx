4461ba6f42674ca8915c6ecfd9f16bfeae527704
om-khorwal <amankhorwal250@gmail.com>
2025-05-09 09:52:00 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,54 @@
-import Image from "next/image";
+'use client';
+import React from "react";
+import axios from "axios";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [anime, setanime] = React.useState<any>(Object);
+  const getQuote = () => {
+    axios.get("https://api.jikan.moe/v4/seasons/2011/spring?sfw")
+      .then((Response) => {
+        const data = Response.data;
+        console.log(data);
+        setanime(data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  }
+  return (
+    <>
+      <div className="   min-h-screen w-full flex flex-col">
+        <div className=" fixed top-0 left-0 w-full h-full bg-cover bg-center -z-10" >
+          <img
+            className="w-full"
+            src="https://images.pexels.com/photos/8137085/pexels-photo-8137085.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Background"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="w-full h-auto flex flex-col items-center justify-start pt-10 z-10 bg-black/70">
+          
+          <div className="text-white flex flex-col items-center justify-center p-10 w-full">
+            <h1 className="text-3xl">Welcome to the world of anime.</h1>
+          </div>
+          <div className="w-full p-2 flex flex-col items-center justify-center">
+            <button onClick={getQuote} className="px-5 py-3 bg-white rounded-2xl active:scale-95">Get Anime data</button>
+          </div>
+          <div className="w-full p-0 flex">
+            <div className="w-full p-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 overflow-y-auto">
+              {
+                anime.data?.map((items: any, index: any) => (
+                  <div key={index} className="bg-white/20 rounded-2xl p-5 m-2 w-full flex flex-col items-center justify-center">
+                    <img src={items.images?.jpg?.image_url} alt={items.images} className="w-full min-h-fit object-cover rounded-lg" />
+                    <h4 className="text-white">{items.title}</h4>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
